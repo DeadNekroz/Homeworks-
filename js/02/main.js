92bbe987255e2745d83cb7675c08cea7f8c2256c
DeadNekroz <nekrok96@gmail.com>
2023-09-11 20:28:39 +0300

    loops removed

--- js/02/main.js
+++ js/02/main.js
@@ -16,20 +16,28 @@ function firstBtnAdd() {
 }
 
 function secondBtnAdd() {
-  let a = "2";
+  let a = prompt('please enter 2')
+  if(a!=Number(2)){
+    alert('no, its my game with my rules, write 2 ')
+    return;}
+    
   let b = 1;
-  let result = Number(a) + b;
+  let result = Number(a) + Number(b);
   console.log(result);
-  alert(`${result},  let a = "2"
+  alert(`${result} and thats how:   let a = prompt('please enter 2')
+  if(a!=Number(2)){
+    alert('no, its my game with my rules, write 2 ')
+    return;}
+    
   let b = 1;
-  let result = Number(a)+b;
-   `);
-}
+  let result = Number(a) + Number(b);
+  console.log(result);`);
+}    
+ 
 
 function thirdBtnFlashSize() {
   let input = prompt("Please, enter your flash memory size in gb");
   let memory = parseInt(input);
-  checkInput = isNaN(input);
   let mbSize = memory * 1000;
   let howManyFiles = Math.round(mbSize / 820);
   console.log(memory);
@@ -41,29 +49,14 @@ function thirdBtnFlashSize() {
 }
 
 function howManyChocolatesYouCanBuy() {
-
-  while (true) {
-    var money = parseInt(prompt("what  are you counting on(in UAH)?"));
-    if (isNaN(money) === false) {
-        
-      break;
-      
-    }
-    alert("Please, check amount");
-  }
-  while (true) {
-    var price = parseInt(prompt(" enter the cost of the chocolate bar here"));
-
-    if (isNaN(price) === false) {
-      
-      break;
-    }
-    alert("Please, enter correct price");
-  }
+  var money = parseInt(prompt("what  are you counting on(in UAH)?"));
+  var price = parseInt(prompt(" enter the cost of the chocolate bar here"))
+  if (!isNaN(money)&&!isNaN(price)){
+     
   console.log(money);
   console.log(price);
   let rest = money % price;
-  let deal = Math.round(money / price);
+  let deal = Math.floor(money / price);
 
   if (money < price) {
     alert("You can't buy chocolate now, go and earn some money");
@@ -72,15 +65,18 @@ function howManyChocolatesYouCanBuy() {
     deal--;
     alert(chocoCount);
   }
+  }else {alert("Please, check amount and price")}
+  
 }
+ 
 function fourtBtnReverseNumber(){
   let reversedNumber='';
-  let number = parseInt(prompt("write number to reverse it "));
+  let number = parseInt(prompt("write 3digit number to reverse it "));
+  if(!isNaN(number)){
   console.log(number);
   var firstNumb=((number)%10);
   console.log(firstNumb)
-  if(firstNumb>5&&secondNumb!=0){
-  }
+ 
   var secondNumb=Math.floor(((number)%100)/10) ;
   if(secondNumb===0){
     secondNumb=Math.floor((number)%10/10);
@@ -88,30 +84,23 @@ function fourtBtnReverseNumber(){
   }
   console.log(secondNumb) 
   var thirdNumb=Math.floor(((number)%1000)/100);
-  if(thirdNumb>5&&secondNumb!=0){
-  } 
+ 
   console.log(thirdNumb)
   reversedNumber=`${firstNumb}${secondNumb}${thirdNumb}`;
-  alert(reversedNumber)
+  alert(reversedNumber)} 
+   
 }
 
 function fifthBtnDepositCalc(){
   var amount = parseInt(prompt('write your deposit amount here '));
-  console.log(amount)
   var interestRate = 5;
-  console.log(interestRate);
   var period = parseInt(prompt('please write period (in month)to calc profit'))
-
+  if(!isNaN(amount)&&!isNaN(period)){
  
- 
-  for (let i=1; i<=period ; i++){
   var afterMonthProfit = ((amount/100)*interestRate)/12 ;
-   amount+=afterMonthProfit;
-  
-  console.log(afterMonthProfit)
+  var result =Math.floor(amount+(afterMonthProfit*period));
  
-  }
-  console.log(amount)
-  return alert(`you will gain ${amount} деняк`)
-  
+  return alert(`you will gain ${result} UAH`)
+} 
+alert(`plz, check amount and period`)
 }
