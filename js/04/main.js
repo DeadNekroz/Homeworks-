60294bcbbec6abb01233e30c451f33e5a8338196
DeadNekroz <nekrok96@gmail.com>
2023-09-18 20:54:36 +0300

    js hw 4 added

--- js/04/main.js
+++ js/04/main.js
@@ -1,3 +1,63 @@
+// #region min
+//  function name() {}
+// () => {}
+// let name = function(){}
+ function argsCount( ){
+  alert ((argsCount.arguments).length)
+ }
+
+ function comparison(){
+    let firstNum= parseInt(prompt('введіть перше число для порівняння'))
+    let secondNum= parseInt(prompt('введіть друге число для порівняння'))
+    switch (true){
+     case  (firstNum<secondNum) :
+      alert(-1)
+      break
+      case  (firstNum>secondNum) :
+      alert(1)
+      break
+      case  (firstNum===secondNum) :
+      alert(0)
+      break
+    }
+ }
+
+ factorial=()=>{
+  let numb1=parseInt(prompt('введіть число для обчислення факторіалу'))
+  let result=0;
+  for(let i=1;i<=numb1;i++){
+  result +=numb1*(numb1-i)
+ }
+ console.log(result)
+ alert(result)
+ }
+
+ numbersInOne=()=>{
+  let someNumb1=parseInt(prompt('введіть цифру для склейки в число'));
+  let someNumb2=parseInt(prompt('ще одну цифру'));
+  let someNumb3=parseInt(prompt('і ще одну цифру'));
+  if(isNaN(someNumb1)||isNaN(someNumb2)||isNaN(someNumb3)){
+    alert('будь ласка, введіть саме цифри') 
+}else{
+let result = Number(`${someNumb1}${someNumb2}${someNumb3}`)
+console.log(result)
+alert(result) 
+}  
+ }
+
+function rectangleSize(){
+  if(rectangleSize.arguments[1]==undefined){
+    alert(`size of this rectangle = ${rectangleSize.arguments[0]**2}`)
+  }
+  else{
+    alert(`size of this rectangle =${rectangleSize.arguments[0]*rectangleSize.arguments[1]}`)
+ }
+}
+
+// #endregion 
+ 
+// #region normal
+
 function ifPurfectNumber(number){
 
   let totalDivider= 0;
@@ -14,7 +74,7 @@ function ifPurfectNumber(number){
 }
 function showResult(){
   let userNumber=parseInt(prompt('введіть будь ласка число'));
-  console.log(ifPurfectNumber(userNumber)? 'number is purfect' :'number is not purfect')
+  alert(ifPurfectNumber(userNumber)? 'number is purfect' :'number is not purfect')
 
 }
 
@@ -29,3 +89,4 @@ function purfectInRange(){
 
   } alert(`${result} are purfect`)
 }
+// #endregion
