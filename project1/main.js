cca4d5b7e909e32dc49111fd88a9f699d429b860
DeadNekroz <nekrok96@gmail.com>
2023-10-03 21:10:16 +0300

    scale added project1

---
+++ project1/main.js
@@ -0,0 +1,15 @@
+function scrollToElement(event){
+event.preventDefault();
+const target = event.target.dataset.href;
+const targetElement = document.querySelector(target);
+const viewportOffset = targetElement.getBoundingClientRect().top;
+
+
+if(targetElement){
+  window.scrollTo({
+    top:viewportOffset,
+    behavior:'smooth'
+  })
+}
+
+}
