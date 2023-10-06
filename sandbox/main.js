1f845061d259d7c1df23ba746a5c9a2fba689ddb
DeadNekroz <nekrok96@gmail.com>
2023-10-06 22:04:13 +0300

    added controller for ligths in trafficL

--- sandbox/main.js
+++ sandbox/main.js
@@ -50,3 +50,27 @@ container.insertAdjacentHTML('beforebegin, newHtml');
 
 console.log(container);
 // #endregion
+
+const actionsConfig ={
+  add:function(){
+    console.log('Add to cart');
+  },
+  remove:function(){
+    console.log('Remove from cart');
+  },
+  clear: function(){
+    console.log('Clear cart');
+  }
+  }
+  
+  
+  
+  function buttonAction(event){
+    const functionKey = event.target.dataset.action;
+    console.log(functionKey);
+    actionsConfig[functionKey]();
+  
+  }
+  document.getElementById('add').addEventListener('click', buttonAction);
+  document.getElementById('remove').addEventListener('click', buttonAction);
+  document.getElementById('clear').addEventListener('click', buttonAction);
