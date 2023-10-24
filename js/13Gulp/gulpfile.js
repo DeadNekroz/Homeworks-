f27dc6f3cd11c368ec0a8b6c9e87f3d66b730b79
R-T <nekrok96@gmail.com>
2023-10-24 19:10:08 +0300

    .

--- js/13Gulp/gulpfile.js
+++ js/13Gulp/gulpfile.js
@@ -1,10 +1,30 @@
-const {src, dest} = require('gulp');
+const { src , dest, watch } = require('gulp');
+const browserSync           = require('browser-sync').create();
+const sass                  = require('gulp-sass')(require('sass'));
+
+function styles(){
+  return src('app/styles/**/*.scss')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(dest('build/css'));
+}
+exports.styles=styles;
+
+function serve(){
+browserSync.init({
+    server: {
+      baseDir:"./build/"
+    }
+});
+  watch('app/**/*.html', moveHtml).on('change', browserSync.reload);
+  watch('app/**/*.scss', styles).on('change', browserSync.reload);
+};
+
+exports.serve=serve;
 
 function moveHtml(){
-  return src('app/index.html')
+  return src('app/**/*.html')
      .pipe(dest('build/'))
 }
+
 exports.moveHtml = moveHtml;
-
-
  
